53de6309dd0a374d1989cebf52bd4f88eecba862
ytsdayou <ytsdayou@gmail.com>
2024-01-17 19:26:54 +0800

    no need extend jest

--- structures/ListNode.js
+++ structures/ListNode.js
@@ -18,15 +18,3 @@ export const ArrayToListNode = function(arr) {
 
     return null;
 }
-
-export const isEqualListNode = function(node1, node2) {
-    while (node1 !== null && node2 !== null) {
-      if (node1.val !== node2.val) {
-        return false;
-      }
-      node1 = node1.next;
-      node2 = node2.next;
-    }
-    // Check if both nodes reach the end at the same time
-    return node1 === null && node2 === null;
-  }
--- test/medium/0002.Add-Two-Numbers/add-two-numbers.test.js
+++ test/medium/0002.Add-Two-Numbers/add-two-numbers.test.js
@@ -7,21 +7,21 @@ describe("0002.Add-Two-Numbers", () => {
         const l1 = ArrayToListNode( [2,4,3] ),
               l2 = ArrayToListNode( [5,6,4] );
         const result = ArrayToListNode( [7,0,8] );
-        expect( addTwoNumbers( l1, l2 ) ).toBeEqualListNode( result );
+        expect( addTwoNumbers( l1, l2 ) ).toEqual( result );
     });
 
     test('Input: l1 = [0], l2 = [0], Output should be [0]', () => {
         const l1 = ArrayToListNode( [0] ),
               l2 = ArrayToListNode( [0] );
         const result = ArrayToListNode( [0] );
-        expect( addTwoNumbers( l1, l2 ) ).toBeEqualListNode( result );
+        expect( addTwoNumbers( l1, l2 ) ).toEqual( result );
     });
 
     test('Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9], Output should be [8,9,9,9,0,0,0,1]', () => {
         const l1 = ArrayToListNode( [9,9,9,9,9,9,9] ),
               l2 = ArrayToListNode( [9,9,9,9] );
         const result = ArrayToListNode( [8,9,9,9,0,0,0,1] );
-        expect( addTwoNumbers( l1, l2 ) ).toBeEqualListNode( result );
+        expect( addTwoNumbers( l1, l2 ) ).toEqual( result );
     });
 
 });
