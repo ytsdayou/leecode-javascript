6e9e9c2470472ae7eb1d5d3345706a845f9f0d8e
ytsdayou <ytsdayou@gmail.com>
2024-01-18 14:50:39 +0800

    0003 solution

---
+++ solution/medium/0003. Longest Substring Without Repeating Characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,31 @@
+/**
+ * We can solve this problem using the sliding window technique.
+ * 1. Initialize two pointers, `slow` and `fast`, to mark the 
+ *    current substring.
+ * 2. Move the `fast` pointer to the end one step at a time, and
+ *    keep track of the characters seen so far using a hashmap.
+ * 3. If a repeating character is found from the hashmap, move `slow`
+ *    pointer to right to shrink the window until the repeating character
+ *    no longer included in the window.
+ * 4. Updated the maximum lenght of the non-repeating substring encountered
+ *    so far during this process.
+ * 5. Repeating steps 2-4 util the `fast` pointer reaches the end of the string
+ * @param {string} s
+ * @return {number}
+ */
+export const lengthOfLongestSubstring = function(s) {
+    let slow = 0, fast = 0, ret = 0;
+
+    const mp = new Map();
+    while( fast < s.length ) {
+        if( mp.has( s[fast] ) && slow <= mp.get( s[fast] ) ) {
+            slow = mp.get( s[fast] ) + 1;
+        }
+
+        mp.set( s[fast], fast );
+        fast++;
+        ret = Math.max( ret, fast - slow );
+    }
+
+    return ret;
+};
---
+++ test/medium/0003. Longest Substring Without Repeating Characters/longest-substring-without-repeating-characters.test.js
@@ -0,0 +1,27 @@
+import { 
+    lengthOfLongestSubstring 
+} from "../../../solution/medium/0003. Longest Substring Without Repeating Characters/longest-substring-without-repeating-characters";
+
+describe("0002.Add-Two-Numbers", () => {
+
+    test('Input: s = "abcabcbb", Output should be 3', () => {
+        expect( lengthOfLongestSubstring( "abcabcbb" ) ).toEqual( 3 );
+    });
+
+    test('Input: s = "bbbbb", Output should be 1', () => {
+        expect( lengthOfLongestSubstring( "bbbbb" ) ).toEqual( 1 );
+    });
+
+    test('Input: s = "pwwkew", Output should be 3', () => {
+        expect( lengthOfLongestSubstring( "pwwkew" ) ).toEqual( 3 );
+    });
+
+    test('Input: s = "a", Output should be 1', () => {
+        expect( lengthOfLongestSubstring( "a" ) ).toEqual( 1 );
+    });
+
+    test('Input: s = "", Output should be 0', () => {
+        expect( lengthOfLongestSubstring( "" ) ).toEqual( 0 );
+    });
+
+})
