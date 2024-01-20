f0bdf666f5a0d6148ce8941467fe07ba9d3cbd9a
ytsdayou <ytsdayou@gmail.com>
2024-01-20 20:58:57 +0800

    0004. find kth smallest

--- solution/hard/0004.Median-of-Two-Sorted-Arrays/median-of-two-sorted-arrays.js
+++ solution/hard/0004.Median-of-Two-Sorted-Arrays/median-of-two-sorted-arrays.js
@@ -49,18 +49,59 @@ export const findMedianSortedArrays = function(nums1, nums2) {
         ret = mergedArr[ d ];
     if( n % 2 === 0 ) {
         ret = ( ( mergedArr[ d ] + mergedArr[ d - 1 ] ) / 2 ).toFixed(5);
-    } 
+    }
 
     return Number(ret);
 };
 
 /**
  * Aprroach 2. find the Kth smallest number. Time Complexity O( log(m+n) )
- * 查找中位数就相当于
+ * The search for the median is essentially a special case of finding the kth smallest number. 
+ * In this problem, the goal is to find the median in two arrays. If the combined length of the 
+ * arrays is odd, the median is the kth element, where k = floor((length+1)/2). If the combined 
+ * length is even, the median is the average of the kth and (k+1)th elements, where k = floor((length+1)/2). 
+ * To find the kth element in the merged array, we can break it down into finding the k/2th smallest 
+ * elements in each of the two arrays. Then, we compare these elements and remove the smaller element 
+ * and the elements before it from their respective arrays. After removing these smaller elements, 
+ * the search task becomes finding the (k - (k/2))th smallest element. This process is repeated until 
+ * one of the arrays becomes empty, or the value of k is equal to 1.
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
  */
 export const findMedianSortedArrays2 = function(nums1, nums2) {
+    let comLen = nums1.length + nums2.length,
+        k = Math.floor( (comLen+1)/2 ),
+        ret = 0;
+
+    if( comLen % 2 === 0 ) { // if odd
+
+        ret = ( ( findKthElement(nums1, 0, nums1.length - 1, nums2, 0, nums2.length - 1, k) 
+                + findKthElement(nums1, 0, nums1.length - 1, nums2, 0, nums2.length - 1, k + 1) ) / 2 ).toFixed(5);
+
+    } else {  //if even
+
+        ret = findKthElement(nums1, 0, nums1.length - 1, nums2, 0, nums2.length - 1, k).toFixed(5);
+
+    }
+
+    return Number(ret);
+}
+
+const findKthElement = function(arr1, start1, end1, arr2, start2, end2, k) {
+    let len1 = end1 - start1 + 1,
+        len2 = end2 - start2 + 1;
     
+    if( len1 === 0 ) return arr2[ start2 + k - 1 ];
+    if( len2 === 0 ) return arr1[ start1 + k - 1 ];
+    if( k === 1 ) return Math.min(arr1[start1], arr2[start2]);
+
+    let index1 = start1 + Math.min(len1, Math.floor(k / 2)) - 1;
+    let index2 = start2 + Math.min(len2, Math.floor(k / 2)) - 1;
+
+    if( arr1[index1] <= arr2[index2] ) {
+        return findKthElement(arr1, index1 + 1, end1, arr2, start2, end2, k - (index1 - start1 + 1) );
+    }
+    
+    return findKthElement(arr1, start1, end1, arr2, index2 + 1, end2, k - (index2 - start2 + 1) );
 }
--- test/hard/0004.Median-of-Two-Sorted-Arrays/median-of-two-sorted-arrays.test.js
+++ test/hard/0004.Median-of-Two-Sorted-Arrays/median-of-two-sorted-arrays.test.js
@@ -1,9 +1,10 @@
 import { 
-    findMedianSortedArrays 
+    findMedianSortedArrays , findMedianSortedArrays2
 } from "../../../solution/hard/0004.Median-of-Two-Sorted-Arrays/median-of-two-sorted-arrays";
 
 describe("0004. Median of Two Sorted Arrays", () => {
 
+    // approach 1
     test('Input: nums1 = [1,3], nums2 = [], Output should be 2.00000', () => {
         expect( findMedianSortedArrays( [1,3], [] ) ).toEqual( 2.00000 );
     });
@@ -18,6 +19,23 @@ describe("0004. Median of Two Sorted Arrays", () => {
 
     test('Input: nums1 = [1,2], nums2 = [3,4], Output should be 2.50000', () => {
         expect( findMedianSortedArrays( [1,2], [3,4] ) ).toEqual( 2.50000 );
+    });
+
+    // approach 2
+    test('Input: nums1 = [1,3], nums2 = [], Output should be 2.00000', () => {
+        expect( findMedianSortedArrays2( [1,3], [] ) ).toEqual( 2.00000 );
+    });
+
+    test('Input: nums1 = [1,3], nums2 = [2], Output should be 2.00000', () => {
+        expect( findMedianSortedArrays2( [1,3], [2] ) ).toEqual( 2.00000 );
+    });
+
+    test('Input: nums1 = [3,4], nums2 = [1,2], Output should be 2.50000', () => {
+        expect( findMedianSortedArrays2( [1,2], [3,4] ) ).toEqual( 2.50000 );
+    });
+
+    test('Input: nums1 = [1,2], nums2 = [3,4], Output should be 2.50000', () => {
+        expect( findMedianSortedArrays2( [1,2], [3,4] ) ).toEqual( 2.50000 );
     });
 
 })
