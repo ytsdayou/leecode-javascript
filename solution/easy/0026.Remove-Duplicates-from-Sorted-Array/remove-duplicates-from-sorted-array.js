c0598a6f411fe2cb5e172b3b9debeaba102c327e
ytsdayou <ytsdayou@gmail.com>
2024-01-22 16:58:38 +0800

    0026 solution

--- solution/easy/0026.Remove-Duplicates-from-Sorted-Array/remove-duplicates-from-sorted-array.js
+++ solution/easy/0026.Remove-Duplicates-from-Sorted-Array/remove-duplicates-from-sorted-array.js
@@ -1,7 +1,55 @@
 /**
+ * We can start by using two pointers, 'slow' and 'fast', both positioned at 
+ * the beginning of the 'nums' array. Initially, we'll increment the fast 
+ * pointer by one step at a time until the element it points to is greater 
+ * than the element pointed to by the slow pointer. Once that happens, we'll 
+ * move the slow pointer ahead by one step. If the element pointed to by the 
+ * slow pointer is less than or equal to the previous one, we should swap the 
+ * elements pointed to by the slow and fast pointers. We'll repeat this process 
+ * until the fast pointer reaches the end of the 'nums' array. The result will 
+ * then be the position of the slow pointer plus one.
  * @param {number[]} nums
  * @return {number}
  */
 export const removeDuplicates = function(nums) {
-    
+    let slow = 0, fast = 0, n = nums.length - 1;
+
+    while( fast <= n ) {
+
+        while( nums[fast] === nums[slow] && fast <= n ) {
+            fast++;
+        }
+
+        if( slow + 1 <= n && fast <= n ) {
+            slow++;
+            if( nums[slow] <= nums[slow-1] ) {
+                const tmp = nums[slow];
+                nums[slow] = nums[fast];
+                nums[fast++] = tmp;
+            }
+        }
+    }
+
+    return slow + 1;
 };
+
+
+/**
+ * Approach learned from the comunity
+ * The approach is similar to mine, but its code is simpler and cleaner than mine,
+ * and also is easy to understand.
+ * @param {number[]} nums
+ * @return {number}
+ */
+export const removeDuplicates2 = function(nums) {
+    let counter = 1;
+
+    for( let i = 1; i < nums.length; i++ ) {
+        if( nums[i] != nums[i-1] ) {
+            nums[counter] = nums[i];
+            counter++;
+        }
+    }
+
+    return counter;
+}
--- test/easy/0026.Remove-Duplicates-from-Sorted-Array/remove-duplicates-from-sorted-array.test.js
+++ test/easy/0026.Remove-Duplicates-from-Sorted-Array/remove-duplicates-from-sorted-array.test.js
@@ -1,8 +1,12 @@
 import { 
-    removeDuplicates 
+    removeDuplicates, removeDuplicates2
 } from "../../../solution/easy/0026.Remove-Duplicates-from-Sorted-Array/remove-duplicates-from-sorted-array";
 
 describe("0001.Two Sum", () => {
+
+    test('Input: nums = [1], Output: 1, nums = [1]', () => {
+        expect( removeDuplicates( [1] ) ).toStrictEqual( 1 );
+    });
 
     test('Input: nums = [1,1,2], Output: 2, nums = [1,2,_]', () => {
         expect( removeDuplicates( [1,1,2] ) ).toStrictEqual( 2 );
@@ -12,4 +16,23 @@ describe("0001.Two Sum", () => {
         expect( removeDuplicates( [0,0,1,1,1,2,2,3,3,4] ) ).toStrictEqual( 5 );
     });
 
+    test('Input: nums = [1,2,3,4,5,5,5,5,5,5], Output: 5, nums = [1,2,3,4,5,_,_,_,_,_]', () => {
+        expect( removeDuplicates( [1,2,3,4,5,5,5,5,5,5] ) ).toStrictEqual( 5 );
+    });
+
+    test('Input: nums = [1], Output: 1, nums = [1]', () => {
+        expect( removeDuplicates2( [1] ) ).toStrictEqual( 1 );
+    });
+
+    test('Input: nums = [1,1,2], Output: 2, nums = [1,2,_]', () => {
+        expect( removeDuplicates2( [1,1,2] ) ).toStrictEqual( 2 );
+    });
+
+    test('Input: nums = [0,0,1,1,1,2,2,3,3,4], Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]', () => {
+        expect( removeDuplicates2( [0,0,1,1,1,2,2,3,3,4] ) ).toStrictEqual( 5 );
+    });
+
+    test('Input: nums = [1,2,3,4,5,5,5,5,5,5], Output: 5, nums = [1,2,3,4,5,_,_,_,_,_]', () => {
+        expect( removeDuplicates2( [1,2,3,4,5,5,5,5,5,5] ) ).toStrictEqual( 5 );
+    });
 });
