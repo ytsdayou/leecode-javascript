32a2d35b3539b46032db9a89b5e99b814dd08b67
ytsdayou <ytsdayou@gmail.com>
2024-01-24 15:51:15 +0800

    0121 O(1) space complexity

--- solution/easy/0121.Best-Time-to-Buy-and-Sell-Stock/best-time-to-buy-and-sell-stock.js
+++ solution/easy/0121.Best-Time-to-Buy-and-Sell-Stock/best-time-to-buy-and-sell-stock.js
@@ -20,3 +20,20 @@ export const maxProfit = function(prices) {
     
     return dp[n - 1][0];
 };
+
+export const maxProfit2 = function(prices) {
+    let n = prices.length;
+    let dp_i_0 = 0, dp_i_1 = -Infinity;
+
+    for( let i = 0; i < n; i++ ) {
+        if( i === 0 ) {
+            dp_i_0 = 0;
+            dp_i_1 = -prices[0];
+        }
+
+        dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+        dp_i_1 = Math.max(dp_i_1, -prices[i]);
+    }
+
+    return dp_i_0;
+}
--- test/easy/0121.Best-Time-to-Buy-and-Sell-Stock/best-time-to-buy-and-sell-stock.test.js
+++ test/easy/0121.Best-Time-to-Buy-and-Sell-Stock/best-time-to-buy-and-sell-stock.test.js
@@ -1,5 +1,5 @@
 import { 
-    maxProfit 
+    maxProfit , maxProfit2
 } from "../../../solution/easy/0121.Best-Time-to-Buy-and-Sell-Stock/best-time-to-buy-and-sell-stock";
 
 describe("0189.Rotate-Array", () => {
@@ -10,6 +10,14 @@ describe("0189.Rotate-Array", () => {
 
     test('Input: prices = [7,6,4,3,1], Output: 0', () => {
         expect( maxProfit([7,6,4,3,1]) ).toEqual( 0 );
+    });
+
+    test('Input: prices = [7,1,5,3,6,4], Output: 5', () => {
+        expect( maxProfit2([7,1,5,3,6,4]) ).toEqual( 5 );
+    });
+
+    test('Input: prices = [7,6,4,3,1], Output: 0', () => {
+        expect( maxProfit2([7,6,4,3,1]) ).toEqual( 0 );
     });
 
 });
