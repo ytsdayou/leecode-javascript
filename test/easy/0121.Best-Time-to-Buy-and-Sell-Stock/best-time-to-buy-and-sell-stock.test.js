0b822681ad66b95a3c268b3ba1b7fa226914ebd7
ytsdayou <ytsdayou@gmail.com>
2024-01-24 15:27:04 +0800

    121 dp table

---
+++ test/easy/0121.Best-Time-to-Buy-and-Sell-Stock/best-time-to-buy-and-sell-stock.test.js
@@ -0,0 +1,15 @@
+import { 
+    maxProfit 
+} from "../../../solution/easy/0121.Best-Time-to-Buy-and-Sell-Stock/best-time-to-buy-and-sell-stock";
+
+describe("0189.Rotate-Array", () => {
+
+    test('Input: prices = [7,1,5,3,6,4], Output: 5', () => {
+        expect( maxProfit([7,1,5,3,6,4]) ).toEqual( 5 );
+    });
+
+    test('Input: prices = [7,6,4,3,1], Output: 0', () => {
+        expect( maxProfit([7,6,4,3,1]) ).toEqual( 0 );
+    });
+
+});
