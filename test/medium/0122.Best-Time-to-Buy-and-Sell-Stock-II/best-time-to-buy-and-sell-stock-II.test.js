1e33c44584bde4996ab2fee5aaf9546ec70bc0c3
ytsdayou <ytsdayou@gmail.com>
2024-01-24 20:09:43 +0800

    0122 O(1) complexity

--- test/medium/0122.Best-Time-to-Buy-and-Sell-Stock-II/best-time-to-buy-and-sell-stock-II.test.js
+++ test/medium/0122.Best-Time-to-Buy-and-Sell-Stock-II/best-time-to-buy-and-sell-stock-II.test.js
@@ -1,5 +1,5 @@
 import { 
-    maxProfit 
+    maxProfit , maxProfit2
 } from "../../../solution/medium/0122.Best-Time-to-Buy-and-Sell-Stock-II/best-time-to-buy-and-sell-stock-II";
 
 describe("0122.Best-Time-to-Buy-and-Sell-Stock-II", () => {
@@ -14,6 +14,18 @@ describe("0122.Best-Time-to-Buy-and-Sell-Stock-II", () => {
 
     test('Input: prices = [7,6,4,3,1], Output: 0', () => {
         expect( maxProfit([7,6,4,3,1]) ).toEqual( 0 );
+    });
+
+    test('Input: prices = [7,1,5,3,6,4], Output: 7', () => {
+        expect( maxProfit2([7,1,5,3,6,4]) ).toEqual( 7 );
+    });
+
+    test('Input: prices = [1,2,3,4,5], Output: 4', () => {
+        expect( maxProfit2([1,2,3,4,5]) ).toEqual( 4 );
+    });
+
+    test('Input: prices = [7,6,4,3,1], Output: 0', () => {
+        expect( maxProfit2([7,6,4,3,1]) ).toEqual( 0 );
     });
 
 });
