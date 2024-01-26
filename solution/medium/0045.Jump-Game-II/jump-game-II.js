57addf15de266ae44e8f2680dda24522f0f70a24
ytsdayou <ytsdayou@gmail.com>
2024-01-26 18:53:58 +0800

    45 greedy algorithm

--- solution/medium/0045.Jump-Game-II/jump-game-II.js
+++ solution/medium/0045.Jump-Game-II/jump-game-II.js
@@ -1,7 +1,34 @@
 /**
+ * greedy algorithm
+ * At each step, we pick a position that lets us jump the farthest, within a 
+ * window of choices. If we end up outside the window, it means we've taken a 
+ * jump. We compare distances between elements in the window and keep track of 
+ * the farthest jump. If the farthest jump goes beyond the end of the numbers 
+ * array, we can figure out the number of steps needed. Doing this helps us 
+ * find the fewest steps required.
  * @param {number[]} nums
  * @return {number}
  */
 export const jump = function(nums) {
-    
+    let maxDistance = 0, 
+        stepEdge = 0,
+        minSteps = 0,
+        n = nums.length;
+
+    //base case
+    if( n === 1 ) return 0;
+
+    for( let i = 0; i < n; i++ ) {
+        maxDistance = Math.max(maxDistance, i + nums[i]);
+        if( maxDistance >= n - 1 ) {
+            return minSteps + 1;
+        }
+
+        if( i === stepEdge ) {
+            stepEdge = maxDistance;
+            minSteps++;
+        }
+    }
+
+    return minSteps;
 };
--- test/medium/0045.Jump-Game-II/jump-game-II.test.js
+++ test/medium/0045.Jump-Game-II/jump-game-II.test.js
@@ -2,6 +2,18 @@ import { jump } from "../../../solution/medium/0045.Jump-Game-II/jump-game-II";
 
 describe("0045.Jump-Game-II", () => {
 
+    test('Input: nums = [0], Output: 0', () => {
+        expect( jump([0]) ).toEqual( 0 );
+    });
+
+    test('Input: nums = [1], Output: 0', () => {
+        expect( jump([1]) ).toEqual( 0 );
+    });
+
+    test('Input: nums = [1,2,1,1,1], Output: 3', () => {
+        expect( jump([1,2,1,1,1]) ).toEqual( 3 );
+    });
+
     test('Input: nums = [2,3,1,1,4], Output: 2', () => {
         expect( jump([2,3,1,1,4]) ).toEqual( 2 );
     });
@@ -9,5 +21,9 @@ describe("0045.Jump-Game-II", () => {
     test('Input: nums = [2,3,0,1,4], Output: 2', () => {
         expect( jump([2,3,0,1,4]) ).toEqual( 2 );
     });
+
+    test('Input: nums = [7,0,9,6,9,6,1,7,9,0,1,2,9,0,3], Output: 2', () => {
+        expect( jump([7,0,9,6,9,6,1,7,9,0,1,2,9,0,3]) ).toEqual( 2 );
+    })
 
 });
