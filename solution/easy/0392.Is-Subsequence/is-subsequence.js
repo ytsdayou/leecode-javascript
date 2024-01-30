a383f4b72432d0e77884a34b2ef315fcfc0c0b25
ytsdayou <ytsdayou@gmail.com>
2024-01-30 20:57:17 +0800

    0392 dp array

--- solution/easy/0392.Is-Subsequence/is-subsequence.js
+++ solution/easy/0392.Is-Subsequence/is-subsequence.js
@@ -27,3 +27,49 @@ export const isSubsequence = function(s, t) {
 
     return false;
 };
+
+/**
+ * dp approach (chatgpt)
+ * The goal is to determine if string 's' is a subsequence of string 't'. To achieve 
+ * this, we can use a dynamic programming approach with a 2D array, where dp[i][j] 
+ * represents whether the first i characters of 's' form a subsequence of the first 
+ * j characters of 't'.
+ * 
+ * We initialize a 2D array, dp, with dimensions (m+1) x (n+1), where m and n are the 
+ * lengths of strings 's' and 't' respectively. We set dp[0][j] to true, as an empty 
+ * string is always a subsequence of any string including an empty string.
+ * 
+ * Then, we iterate through the characters of 's' and 't', and fill in the dp array 
+ * based on the matching characters. If the characters match, we set dp[i][j] to 
+ * dp[i-1][j-1], indicating that the current characters can be matched as part of the 
+ * subsequence. Otherwise, we set dp[i][j] to dp[i][j-1], indicating that we ignore 
+ * the current character in 't' and continue checking the subsequence.
+ * 
+ * Finally, we return the value of dp[m][n], which represents whether the entire 's' 
+ * is a subsequence of 't'.
+ * 
+ * This dynamic programming approach allows us to efficiently determine whether 's' is 
+ * a subsequence of 't' by considering all possible subsequence combinations and efficiently 
+ * storing the results.
+ */
+export const isSubsequence2 = function(s, t) {
+    let m = s.length;
+    let n = t.length;
+    let dp = new Array(m + 1).fill(false).map(() => new Array(n + 1).fill(false));
+
+    for (let j = 0; j <= n; j++) {
+        dp[0][j] = true;
+    }
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (s[i - 1] === t[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = dp[i][j - 1];
+            }
+        }
+    }
+
+    return dp[m][n];
+}
--- test/easy/0392.Is-Subsequence/is-subsequence.test.js
+++ test/easy/0392.Is-Subsequence/is-subsequence.test.js
@@ -1,5 +1,5 @@
 import { 
-    isSubsequence 
+    isSubsequence , isSubsequence2
 } from "../../../solution/easy/0392.Is-Subsequence/is-subsequence";
 
 describe("0392.Is-Subsequence", () => {
@@ -18,6 +18,22 @@ describe("0392.Is-Subsequence", () => {
 
     test('Input: s = "axc", t = "ahbgdc", Output: false', () => {
         expect(isSubsequence("axc", "ahbgdc")).toEqual(false);
+    });
+
+    test('Input: s = "", t = "", Output: true', () => {
+        expect(isSubsequence2("", "")).toEqual(true);
+    });
+
+    test('Input: s = "", t = "ahbgdc", Output: true', () => {
+        expect(isSubsequence2("", "ahbgdc")).toEqual(true);
+    });
+
+    test('Input: s = "abc", t = "ahbgdc", Output: true', () => {
+        expect(isSubsequence2("abc", "ahbgdc")).toEqual(true);
+    });
+
+    test('Input: s = "axc", t = "ahbgdc", Output: false', () => {
+        expect(isSubsequence2("axc", "ahbgdc")).toEqual(false);
     });
 
 });
