b85c912fa6d15a060c7880afa93367f753774278
ytsdayou <ytsdayou@gmail.com>
2024-02-04 23:47:21 +0800

    test case

--- test/hard/0030.Substring-with-Concatenation-of-All-Words/substring-with-concatenation-of-all-words.test.js
+++ test/hard/0030.Substring-with-Concatenation-of-All-Words/substring-with-concatenation-of-all-words.test.js
@@ -1,5 +1,5 @@
 import { 
-    findSubstring 
+    findSubstring , findSubstring2
 } from "../../../solution/hard/0030.Substring-with-Concatenation-of-All-Words/substring-with-concatenation-of-all-words";
 
 describe("0209.Minimum-Size-Subarray-Sum", () => {
@@ -14,6 +14,18 @@ describe("0209.Minimum-Size-Subarray-Sum", () => {
 
     test('Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"], Output: [6,9,12]', () => {
         expect( findSubstring("barfoofoobarthefoobarman", ["bar","foo","the"]) ).toEqual( [6,9,12] );
+    });
+
+    test('Input: s = "barfoothefoobarman", words = ["foo","bar"], Output: [0,9]', () => {
+        expect( findSubstring2("barfoothefoobarman", ["foo","bar"]) ).toEqual( [0,9] );
+    });
+
+    test('Input: s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"], Output: []', () => {
+        expect( findSubstring2("wordgoodgoodgoodbestword", ["word","good","best","word"]) ).toEqual( [] );
+    });
+
+    test('Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"], Output: [6,9,12]', () => {
+        expect( findSubstring2("barfoofoobarthefoobarman", ["bar","foo","the"]) ).toEqual( [6,9,12] );
     });
 
 });
