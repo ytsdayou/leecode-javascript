4ec27eabf2c3d56314be1d5378d3c521556be30e
ytsdayou <ytsdayou@gmail.com>
2024-03-17 11:08:41 +0800

    427 solution

--- solution/medium/0427. Construct-Quad-Tree/solution.js
+++ solution/medium/0427. Construct-Quad-Tree/solution.js
@@ -1,7 +1,33 @@
+import { Node } from "../../../structures/QuadTree";
+
 /**
  * @param {number[][]} grid
  * @return {Node}
  */
 export const construct = function(grid) {
+    return dfs(grid.length, 0, 0);
+
+    function dfs(n, r, c) {
+        let isLeaf = true;
+        for(let i = 0; i < n; i++) {
+            for(let j = 0; j < n; j++) {
+                if( grid[r+i][c+j] !== grid[r][c] ) {
+                    isLeaf = false;
+                    break;
+                }
+            }
+        }
     
+        if( isLeaf ) {
+            return new Node(grid[r][c], true);
+        }
+    
+        n = Math.floor(n / 2);
+        let topLeft = dfs(n, r, c),
+            topRight = dfs(n, r, c + n),
+            bottomLeft = dfs(n, r + n, c),
+            bottomRight = dfs(n, r + n, c + n);
+    
+        return new Node(grid[r][c], false, topLeft, topRight, bottomLeft, bottomRight);
+    }
 };
--- structures/QuadTree.js
+++ structures/QuadTree.js
@@ -7,6 +7,30 @@ export const Node = function(val,isLeaf,topLeft,topRight,bottomLeft,bottomRight)
     this.bottomRight = bottomRight;
 };
 
-export const ArrayToQuadTreeNode = function(arr) {
+export const ArrayToQuadTreeNode = function(grid) {
+    return dfs(grid.length, 0, 0);
+
+    function dfs(n, r, c) {
+        let isLeaf = true;
+        for(let i = 0; i < n; i++) {
+            for(let j = 0; j < n; j++) {
+                if( grid[r+i][c+j] !== grid[r][c] ) {
+                    isLeaf = false;
+                    break;
+                }
+            }
+        }
     
+        if( isLeaf ) {
+            return new Node(grid[r][c], true);
+        }
+    
+        n = Math.floor(n / 2);
+        let topLeft = dfs(n, r, c),
+            topRight = dfs(n, r, c + n),
+            bottomLeft = dfs(n, r + n, c),
+            bottomRight = dfs(n, r + n, c + n);
+    
+        return new Node(grid[r][c], false, topLeft, topRight, bottomLeft, bottomRight);
+    }
 }
