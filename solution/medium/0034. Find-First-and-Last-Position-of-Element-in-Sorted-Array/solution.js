a261c38c3457173b776251d162baf8522dc15cc3
ytsdayou <ytsdayou@gmail.com>
2024-03-20 14:52:08 +0800

    34 solution

--- solution/medium/0034. Find-First-and-Last-Position-of-Element-in-Sorted-Array/solution.js
+++ solution/medium/0034. Find-First-and-Last-Position-of-Element-in-Sorted-Array/solution.js
@@ -4,29 +4,36 @@
  * @return {number[]}
  */
 export const searchRange = function(nums, target) {
-    let arr = [], n = nums.length, l = 0, r = n - 1;
+    let n = nums.length, l = 0, r = n - 1;
 
-    binarySearch(l, r);
-    arr.sort((a, b) => a - b);
-    if( !arr.length ) {
-        return [-1, -1];
-    } else if( arr.length === 1 ) {
-        return [arr[0], arr[0]];
-    } else {
-        return [arr[0], arr[arr.length - 1]];
+    let leftIndex = -1;
+    while(l <= r) {
+        let m = Math.floor((l + r) / 2);
+
+        if( nums[m] < target ) {
+            l = m + 1;
+        } else if( nums[m] > target ) {
+            r = m - 1;
+        } else {
+            leftIndex = m;
+            r = m - 1;
+        }
     }
 
-    function binarySearch(left, right) {
-        if( left > right ) {
-            return ;
-        }
+    let rightIndex = -1;
+    l = 0, r = n - 1
+    while(l <= r) {
+        let m = Math.floor((l + r) / 2);
 
-        const mid = Math.floor((left + right) / 2);
-        if( nums[mid] === target ) {
-            arr.push(mid);
+        if( nums[m] < target ) {
+            l = m + 1;
+        } else if( nums[m] > target ) {
+            r = m - 1;
+        } else {
+            rightIndex = m;
+            l = m + 1;
         }
-
-        binarySearch(left, mid - 1);
-        binarySearch(mid + 1, right);
     }
+
+    return [leftIndex, rightIndex];
 };
